263b2afbf5c3022508e72d5bddbe2675a24d6084
vadimistar <vadimistar@gmail.com>
2023-09-10 00:05:47 +0300

    remove password user from serialization

--- models/user.go
+++ models/user.go
@@ -3,5 +3,5 @@ package models
 type User struct {
 	ID       string `json:"id" dynamo:",hash"`
 	Username string `json:"username" dynamo:""`
-	Password string `json:"password" dynamo:""`
+	Password string `json:"-" dynamo:""`
 }
